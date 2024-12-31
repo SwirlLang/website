f2bdcc615e13aea813104eb85432855855b5a3d6
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2024-12-31 20:59:59 +0530

    feat: rotating gradient shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <main class="flex h-screen flex-col items-center justify-center">
 	<div class="dot-grid"></div>
 	<div class="circle"></div>
+	<div class="gradient-bg"></div>
 	<div class="flex flex-col items-center justify-center">
 		<h2 class="better">
 			Build <span>better</span>
@@ -25,8 +26,8 @@
 	.dot-grid {
 		background-image: radial-gradient(white 1px, transparent 0);
 		background-size: 40px 40px;
-        width: 70rem;
-        height: 70rem;
+        width: 80vw;
+        height: 80vw;
         position: absolute;
         top: 100%;
         left: 50%;
@@ -36,9 +37,9 @@
         z-index: -1;
         animation: dot-grid 3s infinite linear;
 	}
-	div.circle {
-		width: 70rem;
-		height: 70rem;
+	.circle {
+		width: 80vw;
+		height: 80vw;
 		border-radius: 50%;
 		background: linear-gradient(to bottom, rgba(27, 27, 27, 1) 0%, rgba(18, 0, 138, 0.6) 100%);
 		position: absolute;
@@ -46,7 +47,24 @@
 		left: 50%;
 		transform: translate(-50%, -50%);
 		z-index: -2;
-		box-shadow: 0px 0px 100px 70px rgba(164, 16, 218, 0.15);
+	}
+	@keyframes rotate_gradient {
+		100% {
+			transform: translate(-50%, -50%) rotate(360deg)
+		}
+	}
+	.gradient-bg {
+		width: 70vw;
+		height: 70vw;
+		background: linear-gradient(45deg, #442576, #12008a 27% 45%, #7e0e7c 72%, #b442b2 92%);
+		position: absolute;
+		border-radius: 50%;
+		top: 100%;
+		left: 50%;
+		filter: blur(130px);
+		transform: translate(-50%, -50%);
+		animation: rotate_gradient 10s infinite linear;
+		z-index: -3;
 	}
 	h2 {
 		font-size: 6rem;
