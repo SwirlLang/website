733da771208e9462f3bc4dc769082bdb60c9539f
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-01-27 18:57:10 +0530

    fix: remove unused params

--- src/routes/download/+page.ts
+++ src/routes/download/+page.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({ params }) => {
-    redirect(302, `/download/latest`);
+export const load: PageLoad = ({}) => {
+	redirect(302, `/download/latest`);
 };
