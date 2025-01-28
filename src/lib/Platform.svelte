3bc31a747ec61ceefe3e4237726cdd730fc8e822
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-01-28 17:59:43 +0530

    refactor: platform download component
    Refactored platform download options into components for better reusability and easier customization

---
+++ src/lib/Platform.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let {
+		platform = 'Platform',
+		icon = 'lucide:computer',
+		downloads = {
+			installer: { format: '.exe', link: '' },
+			archive: { format: '.zip', link: '' }
+		}
+	} = $props();
+</script>
+
+<div class="{platform} flex min-w-32 flex-col">
+	<iconify-icon {icon} width="96" height="96"></iconify-icon>
+	<span class="translate-y-1/2 rounded-3xl bg-[#fddbd5] px-8 py-2 text-center text-black shadow-md"
+		>{platform}</span>
+	<div
+		class="flex h-32 items-center justify-center rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
+		<div class="flex flex-col gap-2">
+			<div class="flex justify-between">
+				<span class="me-3 text-sm my-auto">Installer</span><span
+					class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
+					>{downloads.installer.format}</span>
+			</div>
+			<div class="flex justify-between">
+				<span class="me-3 text-sm my-auto">Archive</span><span
+					class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
+					>{downloads.archive.format}</span>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/download/[version]/+page.svelte
+++ src/routes/download/[version]/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import 'iconify-icon';
+	import Platform from '$lib/Platform.svelte';
+
 	let { data }: PageProps = $props();
+	let download = {
+		windows: {
+			installer: { format: '.exe', link: '' },
+			archive: { format: '.zip', link: '' }
+		},
+		linux: {
+			installer: { format: '.deb', link: '' },
+			archive: { format: '.tar.gz', link: '' }
+		},
+		macos: {
+			installer: { format: '.dmg', link: '' },
+			archive: { format: '.zip', link: '' }
+		}
+	};
 </script>
 
 <main class="mt-16 flex flex-col">
@@ -12,69 +28,9 @@
 		<span class="build mt-5 self-start rounded-md bg-[#fddbd5] px-2 text-black">Alpha</span>
 	</div>
 	<div class="platforms mt-8 flex h-96 w-full justify-evenly">
-		<div class="windows flex min-w-32 flex-col">
-			<iconify-icon icon="uil:windows" width="96" height="96"></iconify-icon>
-			<span
-				class="translate-y-1/2 rounded-3xl bg-[#fddbd5] px-8 py-2 text-center text-black shadow-md"
-				>Windows</span>
-			<div
-				class="flex h-32 items-center justify-center rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
-				<div class="flex flex-col gap-2">
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Installer</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.exe</span>
-					</div>
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Archive</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.zip</span>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="linux flex min-w-32 flex-col">
-			<iconify-icon icon="uil:linux" width="96" height="96"></iconify-icon>
-			<span
-				class="translate-y-1/2 rounded-3xl bg-[#fddbd5] px-8 py-2 text-center text-black shadow-md"
-				>Linux</span>
-			<div
-				class="flex h-32 items-center justify-center rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
-				<div class="flex flex-col gap-2">
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Installer</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.deb</span>
-					</div>
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Archive</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.tar.gz</span>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="macos flex min-w-32 flex-col">
-			<iconify-icon icon="uil:apple" width="96" height="96"></iconify-icon>
-			<span
-				class="translate-y-1/2 rounded-3xl bg-[#fddbd5] px-8 py-2 text-center text-black shadow-md"
-				>Mac OS</span>
-			<div
-				class="flex h-32 items-center justify-center rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
-				<div class="flex flex-col gap-2">
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Installer</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.dmg</span>
-					</div>
-					<div class="flex justify-between">
-						<span class="me-3 text-sm">Archive</span><span
-							class="flex h-6 w-11 items-center justify-center rounded-md bg-[#fddbd5] text-xs text-black shadow-md"
-							>.zip</span>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Platform platform="Windows" icon="uil:microsoft" downloads={download.windows} />
+		<Platform platform="Linux" icon="uil:linux" downloads={download.linux} />
+		<Platform platform="MacOS" icon="uil:apple" downloads={download.macos} />
 	</div>
 </main>
 
