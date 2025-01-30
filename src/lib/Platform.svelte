292b57d5198c9758a07269acaba2e59026d7e5b2
Mrinmoy <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-01-30 19:51:53 +0530

    refactor: improve layout and styling of download links in Platform component

--- src/lib/Platform.svelte
+++ src/lib/Platform.svelte
@@ -7,16 +7,15 @@
 	<span class="translate-y-1/2 rounded-3xl bg-[#e7c6ff] px-8 py-2 text-center text-black shadow-md"
 		>{platform}</span>
 	<div
-		class="flex h-32 items-center justify-center rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
-		<div class="flex items-center gap-2">
+		class="flex flex-col justify-end h-32 rounded-xl border-[1px] border-[#646464] bg-[#414141] shadow-md">
+		<div class="flex w-full h-20 flex-col items-center justify-center gap-2 px-5">
 			{#each downloads as item}
-				<div class="flex justify-between">
-					<a
-						href={item.url}
-						class="flex h-6 w-14 items-center justify-center rounded-md bg-[#d8a4ff] text-xs text-black shadow-md"
-						>{item.format}</a>
-				</div>
+				<a
+					href={item.url}
+					class="flex h-7 w-full items-center justify-center rounded-md bg-[#d8a4ff] text-sm font-medium text-black shadow-md"
+					>{item.format}</a>
 			{/each}
 		</div>
+		<a href="/docs" class="text-[.7rem] flex justify-center gap-1 text-[#c8c8c8] mb-2 hover:text-[#e2e2e2]"> <iconify-icon icon="uil:external-link-alt" width="14" height="14"></iconify-icon> Install instructions</a>
 	</div>
 </div>
